55b674d80be675d0d490426eba79bba82dae5d87
Aaron Laatsch <icoolkid@hotmail.com>
2021-04-15 18:09:11 -0500

    testing this

--- Maven-demo-project/src/main/java/com/example/Cat.java
+++ Maven-demo-project/src/main/java/com/example/Cat.java
@@ -5,6 +5,8 @@ public class Cat {
 	public static void main(String[] args) {
 		System.out.println("Hi, Cat!!");
 		System.out.println("yes");
+		System.out.println("line");
+		System.out.println("testing");
 
 	}
 
